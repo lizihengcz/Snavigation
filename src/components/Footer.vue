c32f9c0e1111241b7e21fc469affe5a1e735a98f
LZH <60334289+lizihengcz@users.noreply.github.com>
2023-10-08 15:27:06 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,6 @@
     <div class="copyright">
       <span class="site-name">{{ siteName }}</span>
       <span class="year">{{ fullYear }}</span>
-      <span
-        class="anthor"
-        @click="jumpTo(copyrightLink ?? 'https://github.com/imsyy/Snavigation')"
-      >
-        {{ siteAnthor }}
-      </span>
       <span v-if="icp" class="icp" @click="jumpTo('https://beian.miit.gov.cn')">
         {{ icp }}
       </span>
